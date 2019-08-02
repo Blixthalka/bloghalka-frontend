9d206764e56b55c474c7e553335bd5cc742c28a5
Emil Nilsson <emil837@gmail.com>
2019-08-02 21:43:28 +0200

    added some space in articles

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -2,11 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArticleServiceService } from '../article-service.service';
 import { Article } from '../article';
+import { ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ArticleComponent implements OnInit {
   article: Article;
